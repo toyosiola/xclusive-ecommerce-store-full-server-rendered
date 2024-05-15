015f2a31be7204265d89982c9ae414347f7c87d5
Oluwatoyosi Oladipo <oladipooluwatoyosi@yahoo.com>
2024-05-15 09:52:00 +0100

    modify modal style

--- src/app/login/Modal.jsx
+++ src/app/login/Modal.jsx
@@ -7,7 +7,7 @@ export default function Modal({ showModal }) {
       className={`fixed inset-0 grid place-items-center bg-black/30 p-4 duration-200 ${showModal ? "scale-100" : "scale-0"}`}
     >
       <div className="max-w-screen-sm rounded-xl bg-white px-8 py-14">
-        <h3 className="mb-3">Success!</h3>
+        <h3 className="mb-3 text-center text-button1">Success!</h3>
         <p className="mb-4">
           Account created successfully. Please check your email to verify
           account
