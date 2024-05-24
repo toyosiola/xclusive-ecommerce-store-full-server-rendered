42075468ba07b5c2b02a7d882f0fbd9342c833db
Oluwatoyosi Oladipo <oladipooluwatoyosi@yahoo.com>
2024-05-24 14:51:29 +0100

    add disabled state to both cart quantity buttons

--- src/app/products/[productId]/QuantityButton.jsx
+++ src/app/products/[productId]/QuantityButton.jsx
@@ -9,7 +9,7 @@ export default function QuantityButton({ value }) {
       name="action"
       value={value}
       disabled={pending}
-      className={`flex h-11 w-10 items-center justify-center border text-2xl duration-300 ${value === "decrease" ? "rounded-l border-black/50 hover:border-button2 hover:bg-button2 hover:text-text disabled:cursor-not-allowed disabled:opacity-30" : "rounded-r border-button2 bg-button2 text-text hover:border-black/50 hover:bg-transparent hover:text-inherit"}`}
+      className={`flex h-11 w-10 items-center justify-center border text-2xl duration-300 disabled:cursor-not-allowed disabled:opacity-30 ${value === "decrease" ? "rounded-l border-black/50 hover:border-button2 hover:bg-button2 hover:text-text" : "rounded-r border-button2 bg-button2 text-text hover:border-black/50 hover:bg-transparent hover:text-inherit"}`}
     >
       {value === "decrease" ? "-" : "+"}
     </button>
