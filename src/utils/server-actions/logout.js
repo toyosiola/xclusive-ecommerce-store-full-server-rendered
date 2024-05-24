49352fb197822835d8fbccd76539304ed6e09ddc
Oluwatoyosi Oladipo <oladipooluwatoyosi@yahoo.com>
2024-05-24 14:17:13 +0100

    fix logout error if user double clicked on logout. Purge cached user cart on logout

--- src/utils/server-actions/logout.js
+++ src/utils/server-actions/logout.js
@@ -8,11 +8,15 @@ const { redirect } = require("next/navigation");
 export default async function logout() {
   const cookie = cookies();
   const session = cookie.get("session")?.value;
+
+  if (!session) redirect("/login"); // incase user double clicked logout
+
   // purge cached user on logout
   try {
     const payload = jwt.verify(session, process.env.JWT_SECRET);
     revalidateTag(`users/${payload.userId}`); // cached user for session verification
     revalidateTag(`wishlist/user-${payload.userId}`); // cached user wishlist
+    revalidateTag(`cart/user-${payload.userId}`); // cached user cart
   } catch (error) {
     console.error(error);
   }
