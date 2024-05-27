298a3600f474d816e4da0c89f67a48e9752bf202
Oluwatoyosi Oladipo <oladipooluwatoyosi@yahoo.com>
2024-05-27 09:54:04 +0100

    adjust styles for cartQuantityButton for single product

--- src/components/SingleProduct/CartQuantityButton.jsx
+++ src/components/SingleProduct/CartQuantityButton.jsx
@@ -21,7 +21,7 @@ export default function CartQuantityButton({
           ? cartQuantity <= 1 // disable decrease when quantity is 1
           : cartQuantity >= quantityInStock) //disable increase when quantity is equal to quantity in stock
       }
-      className={`h-12 rounded bg-black px-6 text-2xl font-bold duration-300 hover:opacity-70 disabled:cursor-not-allowed disabled:opacity-30 ${decreaseBtn ? "rounded-r-none" : "rounded-l-none"}`}
+      className={`h-12 rounded bg-black px-6 text-2xl font-bold duration-300 hover:opacity-70 disabled:cursor-not-allowed disabled:opacity-30 ${decreaseBtn ? "rounded-r-none px-7" : "rounded-l-none"}`}
     >
       {value === "decrease" ? "-" : "+"}
     </button>
