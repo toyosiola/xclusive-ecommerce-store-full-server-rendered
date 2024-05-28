3902b5f15e7430e478d2e0938367b4aa5e5ce169
Oluwatoyosi Oladipo <oladipooluwatoyosi@yahoo.com>
2024-05-28 17:34:11 +0100

    add hover style to remove from cart button for single product component

--- src/components/SingleProduct/RemoveFromCartButton.jsx
+++ src/components/SingleProduct/RemoveFromCartButton.jsx
@@ -8,7 +8,7 @@ export default function RemoveFromCartButton({ cartQuantity }) {
   return (
     <button
       type="submit"
-      className={`cursor-default rounded pb-1 pl-3 pt-1 text-lg font-semibold italic leading-none text-button2 duration-300 disabled:cursor-not-allowed ${
+      className={`cursor-default rounded pb-1 pl-3 pt-1 text-lg font-semibold italic leading-none text-button2 duration-300 hover:opacity-60 disabled:cursor-not-allowed ${
         cartQuantity
           ? "pointer-events-auto opacity-100 disabled:opacity-60"
           : "pointer-events-none opacity-0"
